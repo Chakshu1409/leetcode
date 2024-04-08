5bda5d7457daf23db38c0e36e0004310625027c2
Chakshu1409 <97241238+Chakshu1409@users.noreply.github.com>
2024-04-08 16:57:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -2,30 +2,34 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int size=students.size();
-        queue<int> student;
-        queue<int> sandwich;
-
+        int zero=0;
+        int one=0;
         for(int i=0; i<size; i++){
-            student.push(students[i]);
-            sandwich.push(sandwiches[i]);
-        }
-        int count=0;
-        while(!student.empty()){
-            if(student.front()==sandwich.front()){
-                student.pop();
-                sandwich.pop();
-                count=0;
+            if(students[i]==1){
+                one++;
             }
             else{
-                int top=student.front();
-                student.pop();
-                student.push(top);
-                count++;
-            }
-            if(count>student.size() + 1){
-                return student.size();
+                zero++;
             }
         }
-        return 0;
+        for(int i=0; i<size; i++){
+            if(sandwiches[i]==1){
+                if(one>0){
+                    one--;
+                }
+                else{
+                    return size-i;
+                }
+            }
+            else{
+                if(zero>0){
+                    zero--;
+                }
+                else{
+                    return size-i;
+                }
+            }
+        }
+        return 0; 
     }
 };
