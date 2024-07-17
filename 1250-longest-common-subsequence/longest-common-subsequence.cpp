330d9ae1706487ad8bd277d487ac1938f21c1c80
Chakshu1409 <97241238+Chakshu1409@users.noreply.github.com>
2024-07-18 01:49:55 +0530

    Time: 63 ms (5.14%) | Memory: 26.3 MB (6.03%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    int helper(string& text1, string& text2, int i1, int i2, vector<vector<int>>& dp){
-        if(i1==text1.size() || i2==text2.size()){
+
+    int helper(string& text1, string& text2, int n, int m, int i1, int i2, vector<vector<int>>& dp){
+        if(i1 == n || i2 == m){
             return 0;
         }
 
@@ -9,24 +10,22 @@ public:
             return dp[i1][i2];
         }
 
+        int first=0;
+
         if(text1[i1] == text2[i2]){
-            return dp[i1][i2] = 1+helper(text1, text2,i1+1,i2+1,dp);
+            first=1+helper(text1,text2,n,m,i1+1,i2+1,dp);
         }
 
-        int first=helper(text1,text2,i1+1,i2,dp);
-        int second=helper(text1,text2,i1,i2+1,dp);
-        int third=helper(text1,text2,i1+1,i2+1,dp);
+        int second=helper(text1,text2,n,m,i1+1,i2,dp);
+        int third=helper(text1,text2,n,m,i1,i2+1,dp);
 
-        return dp[i1][i2]=max(first,max(second,third));
-
+        return dp[i1][i2]=max(first, max(second,third));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
-        int i1=0;
-        int i2=0;
-        int size1=text1.size();
-        int size2=text2.size();
-        vector<vector<int>> dp(size1+1, vector<int>(size2+1,-1));
-        return helper(text1,text2,i1,i2,dp);
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return helper(text1,text2,n,m,0,0,dp);
     }
 };
