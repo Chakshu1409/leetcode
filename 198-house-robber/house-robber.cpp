2c8935da7f5e5c7a6fbd9a99bc9e6fc88839b2a6
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-06-12 15:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (5.96%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,13 +6,13 @@ public:
 
         vector<vector<int>> dp(size, vector<int>(2,0));
 
-        dp[0][0]=0;
-        dp[0][1]=nums[0];
-        cout<<dp[0][0]<<" "<<dp[0][1]<<endl;
+        int first=0;
+        int second=nums[0];
         for(int i=1; i<size; i++){
-            dp[i][1] = max(nums[i] + dp[i-1][0], dp[i-1][1]);
-            dp[i][0] = max(dp[i-1][1], dp[i-1][0]); 
+            int temp = max(nums[i] + first, second);
+            first = max(first, second); 
+            second=temp;
         }
-        return max(dp[size-1][1], dp[size-1][0]);
+        return max(first,second);
     }
 };
