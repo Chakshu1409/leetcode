7048f336ccee3494c97020d7d36415aeeefd2354
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-06-13 12:44:57 +0530

    Time: 27 ms (46.52%) | Memory: 24.1 MB (37.43%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
-    void recurr(vector<int>& nums, int size, int target, int index, int& ans){
-        if(index == size && target == 0){
-            ans++;
-            return;
-        }
-        if(index == size){
-            return;
-        }
-
-        recurr(nums,size, target-nums[index], index+1, ans);
-        recurr(nums, size, target+nums[index], index+1, ans);
-    }
-
+    
     int findTargetSumWays(vector<int>& nums, int target) {
         int size=nums.size();
         int ans=0;
-        recurr(nums,size,target,0,ans);
-        return ans;
+
+        int totalSum=0;
+        for(int i=0; i<size; i++){
+            totalSum+=nums[i];
+        }
+
+        if(target>totalSum || target< 0-totalSum){
+            return 0;
+        }
+        
+        vector<int> dp(2*totalSum + 1,0);
+
+        dp[totalSum-nums[0]]++;
+        dp[totalSum+nums[0]]++;
+
+        // for(int i=0; i<=2*totalSum; i++){
+        //     cout<<dp[i]<<" ";
+        // }
+        // cout<<endl;
+
+        for(int i=1; i<size; i++){
+            
+            vector<int> tempDp(2*totalSum+1,0);
+            for(int j=0; j<=2*totalSum; j++){
+                int realNum=j-totalSum;
+                if(realNum+nums[i]+totalSum <= 2*totalSum){
+                    tempDp[j] += dp[realNum+nums[i]+totalSum];
+                }
+                if(realNum-nums[i]+totalSum >= 0){
+                    tempDp[j] += dp[realNum-nums[i]+totalSum];
+                }
+            }
+            // for(int k=0; k<=2*totalSum; k++){
+            //     cout<<tempDp[k]<<" ";
+            // }
+            // cout<<endl;
+            dp=tempDp;
+        }
+        return dp[target+totalSum];
     }
 };
