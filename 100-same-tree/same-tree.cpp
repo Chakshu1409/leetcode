21f5b88c3edb57cdbe8ed53f41abfaf2dc015a50
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-06-14 22:58:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (47.68%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,20 +15,15 @@ public:
         if(p==NULL && q==NULL){
             return true;
         }
-        if(p!=NULL && q==NULL){
-            return false;
-        }
-        if(p==NULL && q!=NULL){
-            return false;
-        }
-        if(p->val != q->val){
-            return false;
-        }
-        
-        bool left=isSameTree(p->left, q->left);
-        bool right= isSameTree(p->right, q->right);
 
-        return left&&right;
+        if(p==NULL || q==NULL){
+            return false;
+        }
+
+        bool left=isSameTree(p->left,q->left);
+        bool right=isSameTree(p->right,q->right);
+
+        return left&&right&&(p->val == q->val);
 
 
     }
