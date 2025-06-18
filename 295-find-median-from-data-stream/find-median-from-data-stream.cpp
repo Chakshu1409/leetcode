4a93ac927bb1e68efc821394c8df92085a1a1f57
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-06-18 23:05:03 +0530

    Time: 65 ms (46.23%) | Memory: 125.1 MB (15.40%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,78 @@
+class MedianFinder {
+public:
+
+    priority_queue<int,vector<int>, greater<int>> minHeap;
+    priority_queue<int> maxHeap;
+
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        // if(maxHeap.empty()){
+        //     maxHeap.push(num);
+        //     return;
+        // }
+        // if(minHeap.empty()){
+        //     minHeap.push(num);
+        //     return;
+        // }
+
+        // if(maxHeap.size() > minHeap.size()){
+        //     if(num >= maxHeap.top()){
+        //         minHeap.push(num);
+        //     }
+        //     else{
+        //         int top=maxHeap.top();
+        //         maxHeap.pop();
+        //         maxHeap.push(num);
+        //         minHeap.push(top);
+        //     }
+        // }
+        // else{
+        //     if(num >= maxHeap.top() && num<=minHeap.top()){
+        //         maxHeap.push(num);
+        //     }
+        //     else if(num>=maxHeap.top()){
+        //         int top=minHeap.top();
+        //         minHeap.pop();
+        //         maxHeap.push(top);
+        //         minHeap.push(num);
+        //     }
+        //     else{
+        //         maxHeap.push(num);
+        //     }
+        // }
+
+        // Step 1: Add to maxHeap
+        maxHeap.push(num);
+
+        // Step 2: Balance: maxHeap's top must be <= minHeap's top
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        // Step 3: Rebalance: maxHeap may have 1 more element than minHeap
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() != minHeap.size()){
+            return maxHeap.top();
+        }
+
+        int top1=maxHeap.top();
+        int top2=minHeap.top();
+
+        return (top1+top2)/2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
