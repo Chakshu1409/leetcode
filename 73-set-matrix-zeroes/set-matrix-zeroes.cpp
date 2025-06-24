33cde562961a8bb99d33c98741025ca0468b8b84
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-06-25 02:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (24.48%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,30 +4,44 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
 
+        int colZero=1;
+        int rowZero=1;
+
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+            if(matrix[i][0] == 0){
+                colZero=0;
+            }
+        }
+        for(int i=0; i<m; i++){
+            if(matrix[0][i] == 0){
+                rowZero=0;
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
                 if(matrix[i][j] == 0){
-                    for(int k=0; k<n; k++){
-                        if(matrix[k][j] != 0){
-                            matrix[k][j]=-10;
-                        }
-                    }
-                    for(int k=0; k<m; k++){
-                        if(matrix[i][k] != 0){
-                            matrix[i][k]=-10;
-                        }
-                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==-10){
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j]=0;
-                }
+                } 
             }
         }
-
+        if(rowZero==0){
+            for(int i=0; i<m; i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(colZero==0){
+            for(int i=0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+        return;
     }
 };
