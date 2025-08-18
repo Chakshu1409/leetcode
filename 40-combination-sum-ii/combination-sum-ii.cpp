20d6b22466b528a41e407ce1962f96b0212ef08d
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-08-18 22:38:48 +0530

    Time: 3 ms (40.51%) | Memory: 13.9 MB (97.96%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
 
-    void recurr(vector<int>& candidates, int target, int size, int index, vector<vector<int>>& ans, vector<int>& ds){
-
-        if(target==0){
+    void recurr(vector<int>& nums, int target, int size, int index, vector<vector<int>>& ans, vector<int>& ds){
+        
+        if(target == 0){
             ans.push_back(ds);
             return;
         }
-        if(target<0){
+        if(index==size){
             return;
         }
 
         for(int i=index; i<size; i++){
-            if(i!=index && candidates[i]==candidates[i-1]) continue;
-            ds.push_back(candidates[i]);
-            recurr(candidates, target-candidates[i], size, i+1, ans, ds);
-            ds.pop_back();
+            if(i==index || nums[i]!=nums[i-1]){
+                if(nums[i] <= target){
+                    ds.push_back(nums[i]);
+                    recurr(nums, target-nums[i], size, i+1, ans, ds);
+                    ds.pop_back();
+                }
+            }
         }
-
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+
         int size=candidates.size();
         vector<vector<int>> ans;
         vector<int> ds;
-        sort(candidates.begin(), candidates.end());
+
         recurr(candidates, target, size, 0, ans, ds);
 
         return ans;
