066fa5b52ea026e7dd5e892e86094091464e4a28
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-08-18 22:57:51 +0530

    Time: 2 ms (42.63%) | Memory: 10.2 MB (96.73%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    void recur(vector<int>& nums, int size, vector<vector<int>>& ans, vector<int> ds, int index){
-        
-        ans.push_back(ds);
-
+    void recurr(vector<int>& nums, int size, int index, vector<vector<int>>& ans, vector<int>& ds){
+        ans.push_back(ds);    
         for(int i=index; i<size; i++){
-            if(i==index){
+            // recurr(nums, size, i+1, ans, ds);
+            if(i==index || nums[i]!=nums[i-1]){
                 ds.push_back(nums[i]);
-                recur(nums,size,ans,ds,i+1);
+                recurr(nums, size, i+1, ans, ds);
                 ds.pop_back();
-                continue;
-            }
-
-            if(nums[i] != nums[i-1]){
-                ds.push_back(nums[i]);
-                recur(nums,size,ans,ds,i+1);
-                ds.pop_back();
-                continue;
             }
         }
     }
@@ -25,8 +16,9 @@ public:
         int size=nums.size();
         vector<vector<int>> ans;
         vector<int> ds;
-        sort(nums.begin(),nums.end());
-        recur(nums,size,ans,ds,0);
+        sort(nums.begin(), nums.end());
+        recurr(nums, size, 0, ans, ds);
+
         return ans;
     }
 };
