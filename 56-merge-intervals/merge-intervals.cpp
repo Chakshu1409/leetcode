0220d483abe080b8d86775077818410668c40032
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-11-21 22:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (93.79%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int size=intervals.size();
-        sort(intervals.begin(), intervals.end());;;;;
-
-        int i=0;
-        int j=0;
         vector<vector<int>> ans;
-        while(j<size){
-            ans.push_back(intervals[i]);
-            while(j<size){
-                if(ans.back()[1] >= intervals[j][0]){
-                    ans.back()[1]=max(ans.back()[1], intervals[j][1]);
-                    j++;
-                }
-                else{
-                    break;
-                }
-                i=j;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        // for(auto it: ans){
+        //         for(auto ij: it){
+        //             cout<<ij<<" ";
+        //         }
+        //         cout<<endl;
+        //     }
+        //     cout<<endl;
+        //     cout<<ans.back()[1]<<endl;
+        //     cout<<endl;
+        for(int i=1; i<size; i++){
+            if(ans.back()[1] >= intervals[i][0]){
+                ans.back()[1] = max(intervals[i][1], ans.back()[1]);
             }
+            else{
+                // cout<<i<<endl;
+                ans.push_back(intervals[i]);
+            }
+            // for(auto it: ans){
+            //     for(auto ij: it){
+            //         cout<<ij<<" ";
+            //     }
+            //     cout<<endl;
+            // }
+            // cout<<endl;
         }
+
         return ans;
     }
 };
