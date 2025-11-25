6517d50e350a085bef919be3a91481a92dc35233
Chakshu Jhamb <97241238+Chakshu1409@users.noreply.github.com>
2025-11-25 10:59:40 +0530

    Time: 9 ms (18.36%) | Memory: 20.7 MB (9.13%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<vector<int>>> adj;
-        for(auto it: flights){
-            adj[it[0]].push_back({it[1], it[2]});
+        unordered_map<int, vector<vector<int>>> adjency;
+        int size=flights.size();
+
+        for(int i=0; i<size; i++){
+            adjency[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
+        
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
 
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0, 0, src});  // numStops, distance, node
 
-        pq.push({0,0,src});
+        vector<int> ansDist(n, INT_MAX);
+        ansDist[src]=0;
 
-        vector<int> costing(n,1e9);
-        costing[src]=0;
-
-        int ans=INT_MAX;
         while(!pq.empty()){
 
-            int numSteps = pq.top()[0];
-            int cost = pq.top()[1];
-            int node = pq.top()[2];
+            int node=pq.top()[2];
+            int currDist=pq.top()[1];
+            int currentSteps=pq.top()[0];
 
             pq.pop();
-
-            if(node == dst){
-                ans=min(ans, cost);
-            }
-
-            if(numSteps == k+1){
+            if(currentSteps==k+1){
                 continue;
             }
-
-            for(auto it: adj[node]){
-                if(costing[it[0]] > cost+it[1]){
-                    costing[it[0]]= cost+it[1];
-                    pq.push({numSteps+1, costing[it[0]], it[0]});
+            for(auto it: adjency[node]){
+                if(currDist + it[1] < ansDist[it[0]]){
+                    ansDist[it[0]]=currDist + it[1];
+                    pq.push({currentSteps+1, ansDist[it[0]], it[0]});
                 }
             }
+
         }
-        if(ans == INT_MAX){
+        if(ansDist[dst] == INT_MAX){
             return -1;
         }
-        return ans;
+        return ansDist[dst];
     }
 };
